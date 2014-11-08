98809f0e92070c72ae248bf98ef4fc5e0a3972b5
Kajaste <Kajaste@users.noreply.github.com>
2014-11-08 22:49:45 +0200

    fizzbuzz: Make test cooler

--- fizzbuzz/fizzbuzz_test.py
+++ fizzbuzz/fizzbuzz_test.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python
 
+'''
+Tests for Fizz Buzz
+'''
+
 import unittest
 import fizzbuzz
 
 
 class FizzBuzzTestCase(unittest.TestCase):
-    def runTest(self):
-        ''' Test that the solution works as intended '''
+    ''' Test case for Fizz Buzz '''
+    def test_word_pick(self):
+        ''' Test that correct output is picked for numbers '''
+        self.assertEqual(fizzbuzz.fizz_buzz_or_fizzbuzz(1), '1')
+        self.assertEqual(fizzbuzz.fizz_buzz_or_fizzbuzz(3), 'Fizz')
+        self.assertEqual(fizzbuzz.fizz_buzz_or_fizzbuzz(5), 'Buzz')
+        self.assertEqual(fizzbuzz.fizz_buzz_or_fizzbuzz(15), 'Fizz Buzz')
+        self.assertEqual(fizzbuzz.fizz_buzz_or_fizzbuzz(16), '16')
 
+    def test_string_construction(self):
+        ''' Test that output string is constructed correctly '''
         expected_36 = '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, ' \
         'Fizz, 13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, ' \
         'Fizz, Buzz, 26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz'
-
         self.assertEqual(fizzbuzz.fizzbuzz(36), expected_36)
 
 
+def main():
+    ''' Set up the framework with tests and run them '''
+    runner = unittest.TextTestRunner()
+    suite = unittest.makeSuite(FizzBuzzTestCase, 'test')
+    runner.run(suite)
+
+
 if __name__ == '__main__':
-    unittest.main()
+    main()
