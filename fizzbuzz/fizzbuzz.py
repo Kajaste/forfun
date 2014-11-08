e2f3b7b416bd3db89d9555a4427c4278fce4ac84
Kajaste <Kajaste@users.noreply.github.com>
2014-11-08 21:30:19 +0200

    fizzbuzz: Put some comments in to satisfy pylint

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,28 +1,37 @@
 #!/usr/bin/env python
 
+'''
+Baby code solution for Fizz Buzz
+'''
+
 import sys
 
 
 def fizz_buzz_or_fizzbuzz(number):
-    divisible_by_3 = not number % 3
-    divisible_by_5 = not number % 5
+    ''' Pick Fizz, Buzz, FizzBuzz or just the number '''
 
-    if divisible_by_3 and divisible_by_5:
+    def divisible_by(divisor):
+        ''' Check whether the number is divisible by the divisor or not '''
+        return number % divisor == 0
+
+    if divisible_by(15):
         return 'Fizz Buzz'
-    elif divisible_by_3:
+    elif divisible_by(3):
         return 'Fizz'
-    elif divisible_by_5:
+    elif divisible_by(5):
         return 'Buzz'
     else:
         return str(number)
 
 
 def fizzbuzz(how_many):
+    ''' Formulate the result string '''
     return ', '.join([fizz_buzz_or_fizzbuzz(i)
                       for i in xrange(1, how_many + 1)])
 
 
 def main():
+    ''' Get the number of items to print or tell the user to provide it '''
     try:
         print fizzbuzz(int(sys.argv[1]))
     except (IndexError, ValueError):
@@ -30,11 +39,12 @@ def main():
 
 
 def test():
-    EXPECTED_36 = '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, ' \
+    ''' Test that the solution works as intended '''
+    expected_36 = '1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, ' \
     '13, 14, Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, ' \
     '26, Fizz, 28, 29, Fizz Buzz, 31, 32, Fizz, 34, Buzz, Fizz'
 
-    assert fizzbuzz(36) == EXPECTED_36
+    assert fizzbuzz(36) == expected_36
 
 
 if __name__ == '__main__':
