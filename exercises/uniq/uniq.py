679c811c8053a5d140707fc6d3046cf76f654398
Kajaste <Kajaste@users.noreply.github.com>
2014-12-12 21:27:20 +0200

    exercises: even better score in uniq

--- exercises/uniq/uniq.py
+++ exercises/uniq/uniq.py
@@ -2,5 +2,5 @@ import itertools as t,sys
 e=enumerate
 s=sorted
 c=s([(x,len(list(y)))for x,y in t.groupby(s(sys.stdin.read()))],key=lambda x:(x[1],x[0]))
-for n,i in e(c,1):print n,repr(i[0])[1:-1]if i[0]!='\\'else'\\',i[1]
-print sum([(i*c[1])for i,c in e(c,1)])
+for n,b in e(c,1):print n,repr(b[0])[1:-1]if b[0]!='\\'else'\\',b[1]
+print sum([(b*c[1])for b,c in e(c,1)])
