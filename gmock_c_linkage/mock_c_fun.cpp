c9ab79f62bc1baff12eb0c8c487d49014c2912c8
Kajaste <Kajaste@users.noreply.github.com>
2015-02-14 12:01:05 +0200

    gmock_c_linkage: Documentation added

--- gmock_c_linkage/mock_c_fun.cpp
+++ gmock_c_linkage/mock_c_fun.cpp
@@ -1,3 +1,35 @@
+//
+// Howto use these:
+//
+// MOCK_METHOD<N>(<NAME>, <RETURN_TYPE>(<T1>, ...<T<N>>));
+//
+// If RETURN_TYPE is not void =>
+//   MOCK_FUNCTION(<MOCK>, <NAME>, <RETURN_TYPE>, <N>, <T1>, ...<T<N>>)
+// Else =>
+//   VOID_MOCK_FUNCTION(<MOCK>, <NAME>, <N>, <T1>, ...<T<N>>)
+//
+
+//
+// Examples
+//
+// VOID_MOCK_FUNCTION(*mock, doBar, 0)
+// MOCK_FUNCTION(mock, doBar, size_t, 0)
+// VOID_MOCK_FUNCTION(mock, doFoo, 2, char*, size_t)
+// MOCK_FUNCTION(*mock, doFoo, int, 2, char*, size_t)
+
+//
+// "Public API"
+//
+#define VOID_MOCK_FUNCTION(MOCK, NAME, PARAM_COUNT, ...) \
+    C_FUN_MOCK##PARAM_COUNT(MOCK, NAME, , void, ##__VA_ARGS__)
+
+#define MOCK_FUNCTION(MOCK, NAME, RETURN_TYPE, PARAM_COUNT, ...) \
+    C_FUN_MOCK##PARAM_COUNT(MOCK, NAME, return, RETURN_TYPE, ##__VA_ARGS__)
+
+
+//
+// Parameter count specific macros
+//
 #define C_FUN_MOCK0(MOCK, NAME, RETURN, RETURN_TYPE) \
     extern "C" RETURN_TYPE NAME(void) { \
         RETURN (MOCK).NAME(); \
@@ -34,14 +66,3 @@
     extern "C" RETURN_TYPE NAME(T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6) { \
         RETURN (MOCK).NAME(p1, p2, p3, p4, p5, p6); \
     }
-
-#define VOID_MOCK_FUNCTION(MOCK, NAME, PARAM_COUNT, ...) \
-    C_FUN_MOCK##PARAM_COUNT(MOCK, NAME, , void, ##__VA_ARGS__)
-
-#define MOCK_FUNCTION(MOCK, NAME, RETURN_TYPE, PARAM_COUNT, ...) \
-    C_FUN_MOCK##PARAM_COUNT(MOCK, NAME, return, RETURN_TYPE, ##__VA_ARGS__)
-
-VOID_MOCK_FUNCTION(*mock, doBar, 0)
-MOCK_FUNCTION(mock, doBar, size_t, 0)
-VOID_MOCK_FUNCTION(mock, doFoo, 2, char*, size_t)
-MOCK_FUNCTION(*mock, doFoo, int, 2, char*, size_t)
